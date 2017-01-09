db756a649384f7ed7caa7423b19ec80537e702af
zhangwxgo@gmail.com <Zwx19930301>
2017-01-09 16:51:06 +0800

    Initial commit

---
+++ app/src/main/java/com/zhangwx/argbshift/MainActivity.java
@@ -0,0 +1,46 @@
+package com.zhangwx.argbshift;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.zhangwx.argbshift.Utils.ViewUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setSupportActionBar(ViewUtils.$(this, R.id.toolbar));
+//        initFab();
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initFab() {
+        ViewUtils.$(this, R.id.fab).setOnClickListener(this::actShift);
+    }
+
+    private void actShift(View view) {
+
+    }
+}
